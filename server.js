e773ccb71b4bb1368d54a7d1f116329bf4e90dbd
Remulo Costa <remulo.costa@gmail.com>
2019-12-26 00:26:42 -0300

    Instalação do conexão com mongodb

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 require('dotenv/config');
 const express = require('express');
+const mongoose = require('mongoose');
 
+/**
+ * iniciando o App
+ */
 const app = express();
 
+/**
+ * iniciando o banco de dados mogodb
+ */
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+/**
+ * iniciando as rotas
+ */
 app.get('/', (req, res) => {
   res.send(process.env.MONGO_URL);
 });
 
+/**
+ * startando servidor na porta indicada
+ */
 app.listen(3000);
