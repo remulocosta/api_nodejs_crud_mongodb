71c89f4cc6397d67afd5cc65e3cd9861fa129cdd
Remulo Costa <remulo.costa@gmail.com>
2019-12-26 01:54:59 -0300

    finalização do CRUD e paginação

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -9,14 +9,43 @@
 
   module.exports = {
     async index(req, res) {
-      const products = await Product.find();
+      const { page = 1 } = req.query;
+      const products = await Product.paginate({}, {page, limit: 5});
 
       return res.json(products);
+    },
+
+    async show(req, res) {
+      const { id } = req.params;
+      const product = await Product.findById(id);
+
+      if(!product) {
+        return res.status(400).json({Error: "Produdo não localizado"});
+      }
+
+      return res.json(product);
     },
 
     async store(req, res) {
       const product = await Product.create(req.body);
 
       return res.json(product);
-    }
+    },
+
+    async update(req, res) {
+      const { id } = req.params;
+      //const { title, description, url } = req.body;
+      const product = await 
+        Product.findByIdAndUpdate(id, req.body, {new: true});
+        //Product.findByIdAndUpdate(id, {title, description, url}, {new: true});
+
+      return res.json(product);
+    },
+
+    async destroy(req, res) {
+      const { id } = req.params;
+      await Product.findByIdAndRemove(id);
+
+      return res.json({OK: "Registro removido"});
+    },
   }
