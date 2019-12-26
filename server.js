71be97608d63cc4e8a110f8aa2030e8d974b0c3b
Remulo Costa <remulo.costa@gmail.com>
2019-12-26 00:14:01 -0300

    Instalação do dotenv

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+require('dotenv/config');
 const express = require('express');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("hello word!");
+  res.send(process.env.MONGO_URL);
 });
 
 app.listen(3000);
