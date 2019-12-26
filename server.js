fdd71a363d65685c6838a5b3b34253046f247a26
Remulo Costa <remulo.costa@gmail.com>
2019-12-26 02:04:45 -0300

    instalação do cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 require('dotenv/config');
 const express = require('express');
-const mongoose = require('mongoose');
+const cors = require('cors');
+const mongoose = require('mongoose'); 
 const requireDir = require('require-dir');
 /**
  * iniciando o App
  */
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 /**
  * iniciando o banco de dados mogodb
